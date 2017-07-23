f3355a718aa457e65e1d29559e166a66caa3763d
zxs107020 <zxs107020@utdallas.edu>
2017-07-23 11:51:26 -0500

    Update CustomerRewardsQueries.sql

--- customer_relations_database/CustomerRewardsQueries.sql
+++ customer_relations_database/CustomerRewardsQueries.sql
@@ -1,4 +1,4 @@
-use group14finalproject;
+use CustomerRewards;
 
 -- Q1: Retrieve the names of all products in inventory
 select name 
@@ -9,8 +9,8 @@ select price*quantity as total_amount
 from sales;
 
 -- Q3: Find the Company ID for the Customer Connect company
-select idCompany 
-from Company
+select idcompany 
+from company
 where 'Customer Connect' = name;
 
 -- Q4: Retrieve all information on the company for whom Zan is listed as the contact
@@ -19,54 +19,54 @@ from company
 where contact = 'Zan';
 
 -- Q5: Return names of all Customer Connect shoppers
-select FirstName, LastName
-from Customers as C, Company as Co
-where c.idCompany=co.idcompany and co.name='Customer Connect';
+select firstname, lastname
+from customers as c, company as co
+where c.idcompany=co.idcompany and co.name='Customer Connect';
 
 -- Q6: Find out which customers have points
-select distinct idCustomer
-from Points
-where Points > 0;  
+select distinct idcustomer
+from points
+where points > 0;  
 
 -- Q7: Find out how many points each customer has
 select
-    t1.FirstName,
-    t1.LastName,
-    t2.Points
+    t1.firstname,
+    t1.lastname,
+    t2.points
 from 
-	(Customers as t1 inner join Points as t2 on t1.idCustomers=t2.idCustomer);
+	(customers as t1 inner join points as t2 on t1.idcustomers=t2.idcustomer);
 
 -- Q8: Retrieve information on which rewards have been sent to which customers
 select
-	c.idCompany,
-	c.FirstName,
-	c.LastName,
+	c.idcompany,
+	c.firstname,
+	c.lastname,
     r.idRewardsCatalog	
 from
  (customers c left join rewards r on c.idCustomers = r.idCustomers);
 
 -- Q9: Find the total number of points earned by each customer
 select
-	idCustomer,
+	idcustomer,
     SUM(points) AS lifetimePoints
 from
-	Points
-group by idCustomer;
+	points
+group by idcustomer;
 
 -- Q10: Find the customers that have earned more than 1000 points and order them by Customer ID
 select
-	idCustomer,
+	idcustomer,
     SUM(points) AS lifetimePoints
 from
-	Points
-group by idCustomer
+	points
+group by idcustomer
 having lifetimePoints > 1000;
 
 -- Q11: Retrieve the first and last name of each customer that has recieved "Survey 1"
-select c.LastName, c.FirstName
-from Customers as c, Surveys_has_Customers as s
-where c.idCustomers=s.idCustomers and s.idSurveys in (select idSurveys
-													  from Surveys as s1
-													  where s1.Name = 'Survey 1');
+select c.lastname, c.firstname
+from customers as c, surveys_has_customers as s
+where c.idcustomers=s.idcustomers and s.idsurveys in (select idsurveys
+						      from surveys as s1
+						      where s1.Name = 'Survey 1');
                                                
 drop schema group14finalproject;
